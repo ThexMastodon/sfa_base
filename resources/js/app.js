88cbe763fd673bb46190ef9cf459b1c0d358c3d3
edgarmorales <dev.edgarmorales@gmail.com>
2023-10-26 11:06:10 -0600

    Add - Integracion de vue3, laravel-permissions(spatie), y configuraciones iniciales.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+import app from './main.js';
+
+app.mount(".wrapper");
+
+$(document).ready(function () {
+  $('.select2').select2({
+    dropdownParent: $('#create-modal, #edit-modal .modal-body'),
+    theme: 'bootstrap4',
+    width: '100%'
+  });
+});
